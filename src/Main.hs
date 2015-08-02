851cc05570a73d558fb6e959d70e9c9e7f995286
liscju <piotr.listkiewicz@gmail.com>
2015-08-02 19:37:31 +0200

    Refactored main function to do action in doAction function

--- src/Main.hs
+++ src/Main.hs
@@ -9,13 +9,11 @@ parseArguments :: [String] -> Maybe Action
 parseArguments ["--version"] = Just GetVersion
 parseArguments badArgList = Nothing
 
+doAction :: Maybe Action -> IO ()
+doAction (Just GetVersion) = putStrLn "DarkHs version 0.1.0.0"
+doAction Nothing = putStrLn "Unrecognized command line arguments"
+
 main :: IO ()
-main =
-    do
-        args <- getArgs
-        let actionToDo = parseArguments args
-        case actionToDo of
-            Just GetVersion -> putStrLn "DarkHs version 0.1.0.0"
-            Nothing -> putStrLn "Unrecognized command line arguments"
+main = getArgs >>= doAction . parseArguments
 
 
