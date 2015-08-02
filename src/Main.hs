3f6cecca9ccf3d1913ec528bb814606fe7ef9d88
liscju <piotr.listkiewicz@gmail.com>
2015-08-02 20:13:15 +0200

    Added init repository action and command line argument

--- src/Main.hs
+++ src/Main.hs
@@ -1,23 +1,76 @@
 module Main where
 
 import System.Environment
+import System.Directory
+import Control.Exception
+import System.FilePath
+import Control.Monad
+import System.IO
 
 data Action =
-    GetVersion
+    Init
+    | GetVersion
     | Usage
 
 parseArguments :: [String] -> Maybe Action
+parseArguments ["init"] = Just Init
 parseArguments ["--version"] = Just GetVersion
 parseArguments ["--help"] = Just Usage
 parseArguments [] = Just Usage
 parseArguments badArgList = Nothing
 
 doAction :: Maybe Action -> IO ()
+doAction (Just Init) = initialize_repository
 doAction (Just GetVersion) = putStrLn "DarkHs version 0.1.0.0"
 doAction (Just Usage) = putStrLn "Usage: DarkHs.exe [--version|--help]"
 doAction Nothing = putStrLn "Unrecognized command line arguments"
 
+initial_directories :: [[FilePath]]
+initial_directories = [
+        [".darkhs"],
+        [".darkhs","files"],
+        [".darkhs","commits"],
+        [".darkhs","trees"]
+    ]
+
+initialize_repository :: IO ()
+initialize_repository =
+    catch
+        (do
+            forM initial_directories $ createDirectory . joinPath
+            withFile (joinPath [".darkhs","HEAD"]) AppendMode
+                (flip hPutStr "0")
+            putStrLn "Initialize repository in .darkhs")
+        (\exc ->
+            putStrLn $ "Initialize failed with exception :" ++
+                            show (exc :: IOException))
+
+
 main :: IO ()
 main = getArgs >>= doAction . parseArguments
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
