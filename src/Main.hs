4b764d38c2c93493d9bd86acfe8c39c928440cdc
liscju <piotr.listkiewicz@gmail.com>
2015-08-02 19:32:00 +0200

    Added parsing of getVersion and printing unrecognized command line argument

--- src/Main.hs
+++ src/Main.hs
@@ -2,7 +2,20 @@ module Main where
 
 import System.Environment
 
+data Action =
+    GetVersion
+
+parseArguments :: [String] -> Maybe Action
+parseArguments ["--version"] = Just GetVersion
+parseArguments badArgList = Nothing
+
 main :: IO ()
 main =
-    putStrLn "DarkHs version 0.1.0.0"
+    do
+        args <- getArgs
+        let actionToDo = parseArguments args
+        case actionToDo of
+            Just GetVersion -> putStrLn "DarkHs version 0.1.0.0"
+            Nothing -> putStrLn "Unrecognized command line arguments"
+
 
