450f82e24d18b1bb64f3c57f51f5e12ee0f2bd83
liscju <piotr.listkiewicz@gmail.com>
2015-08-02 19:41:19 +0200

    Introduced showing usage feature

--- src/Main.hs
+++ src/Main.hs
@@ -4,13 +4,17 @@ import System.Environment
 
 data Action =
     GetVersion
+    | Usage
 
 parseArguments :: [String] -> Maybe Action
 parseArguments ["--version"] = Just GetVersion
+parseArguments ["--help"] = Just Usage
+parseArguments [] = Just Usage
 parseArguments badArgList = Nothing
 
 doAction :: Maybe Action -> IO ()
 doAction (Just GetVersion) = putStrLn "DarkHs version 0.1.0.0"
+doAction (Just Usage) = putStrLn "Usage: DarkHs.exe [--version|--help]"
 doAction Nothing = putStrLn "Unrecognized command line arguments"
 
 main :: IO ()
